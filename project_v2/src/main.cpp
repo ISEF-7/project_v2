e453774f80d257cd6af6c679816608b653c1d821
jkhdr <redhydratechnologies@gmail.com>
2022-12-22 22:48:39 -0500

    Library Descriptions

--- project_v2/src/main.cpp
+++ project_v2/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <ArduinoSTL.h> //https://registry.platformio.org/libraries/mike-matera/ArduinoSTL 
+#include <ArduinoJson.h> //https://registry.platformio.org/libraries/bblanchon/ArduinoJson 
+#include <lc-addrlabels.h> //https://registry.platformio.org/libraries/gitlab-airbornemint/Protothreads 
 
 void setup() {
-  // put your setup code here, to run once:
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
