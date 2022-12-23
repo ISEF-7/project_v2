d90bf457a5b60d07b9724c41168c3957c98c1ce1
jkhdr <redhydratechnologies@gmail.com>
2022-12-23 12:17:48 -0500

    a

--- project_v2/src/main.cpp
+++ project_v2/src/main.cpp
@@ -7,8 +7,7 @@
 
 using namespace std;
 
-
-//c
+///////
 
 class hub{
   public:
@@ -24,43 +23,114 @@ class RoadData{
 class sn_instrc{
   public:
   int instruction_id;
-  string instruction;
+  vector<string> instruction;
 };
 
 class intrp_instrc{
   public:
   int intp_id;
-  string instruction;
+  vector<string> instruction;
+};
+
+class node : location{
+  public:
+
+};
+
+class road{
+  public:
+    float length;
+    vector<node> body;
+    vector<int> lanes;
+    float speedLimit; 
 };
 
 
-//
+class location{
+  public:
+    string name;
+    int node_position;
+};
 
-//modules
+struct roadData{
+  vector<vector<float>> adjmtx_data;
+  location i_location;
+  location destination;
+};
+
+
+
+///////
 
 RPLidar l;
+RPLidar* L = &l;
 Servo m1;
+Servo* M1 = &m1;
 Servo m2;
+Servo* M2 = &m2;
 hub Hub;
+hub* HUB = &Hub;
 
-//
-string servo_moduleCheck(string modulename){
-  if (modulename == "m1"){
+///////
+
+string servo_moduleCheck(Servo servo){
+  //XXX test if comparing objects by runtime memory address works, alternative is to do nested classes, and the base class has the name
+  if (&servo == &m1){
     if (m1.attached() == true){return "OK"; }
     else{return "ERR";}
   }
-  if (modulename == "m2"){
+  if (&servo == &m2){
     if (m2.attached() == true){return "OK";}
     else{return "ERR";}
   }
   return "NULL";
 }
-string lidar_moduleCheck(string modulename){}; //TODO 
-string hub_moduleCheck(string modulename){}; //TODO 
+string lidar_moduleCheck(RPLidar lidar){
+  rplidar_response_device_info_t info;
+  if (IS_OK(lidar.getDeviceInfo(info, 100)) == true && IS_OK(lidar.waitPoint()) == true){
+    return "OK";
+  }
+  return "ERR";
+}; 
+string hub_moduleCheck(hub h){
+}; //TODO 
+
+///////
+
+void boot_servo(vector<Servo> servolist){
+  for (int i=0; i<servolist.size(); i++){
+    cout << "Booting Servo m" << i << " ...";
+    servo_moduleCheck(servolist.at(i));
+  };
+}
+void boot_lidar(RPLidar lidar){
+  cout << "Booting RPLidar...";
+  lidar_moduleCheck(lidar);
+
+  //TODO object configuration
+}
+void boot_hub(hub h){
+  cout << "Booting hub...";
+  hub_moduleCheck(h);
+
+  //TODO object configuration
+}
+
+///////
+
+
+
+
+///////
 
 void setup() {
   cout << "Booting";
+  boot_hub(Hub);
+  boot_lidar(l);
+  boot_servo({m1,m2});
 }
+
+///////
 
 void loop() {
 
