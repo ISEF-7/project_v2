6999317dc6db6d015894981fde09b15cb2fbbd2d
jkhdr <redhydratechnologies@gmail.com>
2022-12-22 19:35:59 -0500

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <Arduino.h>
-
 void setup() {
   // put your setup code here, to run once:
 }
+class hub{
+  public:
+  float wheelsize = 3;
+  float width = 5;
+  //string version = "3";
+  class m1{
+
+  };
+  class m2{
+
+  };
+  class l{
+
+  };
+
+  class specs{
+
+  };
+};
+//road data 
+class _roadData{
+  public:
+    int size;
+    int startnode;
+    int endnode; 
+};
+
+_roadData roadData; 
+
 
 void loop() {
   // put your main code here, to run repeatedly:
