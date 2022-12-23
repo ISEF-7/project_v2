d369ec91f0fd52318a47c5906c1abb5a84e792d9
jkhdr <redhydratechnologies@gmail.com>
2022-12-23 09:18:27 -0500

    Library

--- project_v2/src/main.cpp
+++ project_v2/src/main.cpp
@@ -2,9 +2,24 @@
 #include <ArduinoSTL.h> //https://registry.platformio.org/libraries/mike-matera/ArduinoSTL 
 #include <ArduinoJson.h> //https://registry.platformio.org/libraries/bblanchon/ArduinoJson 
 #include <lc-addrlabels.h> //https://registry.platformio.org/libraries/gitlab-airbornemint/Protothreads 
+#include <RPLidar.h> //https://github.com/robopeak/rplidar_arduino
+#include <Servo.h> //https://registry.platformio.org/libraries/arduino-libraries/Servo
+
+using namespace std;
+
+RPLidar l;
+
+class RoadData{
+};
+
+class motorinstructions{
+  int instruction_id;
+  string instruction;
+};
+
 
 void setup() {
-
+  cout << "s"; 
 }
 
 void loop() {
